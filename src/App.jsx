804b1bdb2edda99deecc31e6fb9e6de4839a94df
kartikpadal <kartikpadal17@gmail.com>
2025-07-02 17:42:17 +0530

    started ringify project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Hero from './Hero.jsx'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <Hero/>
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/Hero.jsx
@@ -0,0 +1,12 @@
+function Hero() {
+  return (
+    <div className="hero">
+      <h1 className="hero-title">Ringify</h1>
+      <p className="hero-description">
+        Instantly get your favorite ringtones in .mp3 format. 
+      </p>
+    </div>
+  );
+}
+
+export default Hero;
