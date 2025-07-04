bc7c8729445d50a0ad6188db001d8228e23fdc49
kartikpadal <kartikpadal17@gmail.com>
2025-07-04 18:06:34 +0530

    made search box today

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import Hero from './Hero.jsx'
+import SearchBar from './SearchBar.jsx'
 import './App.css'
 
 function App() {
   
+  const handleLinkSubmit = (link) => {
+    console.log("User pasted:", link);
+    // future: send to API or backend
+  };
+
   return (
     <>
       <Hero/>
-        
+      <SearchBar onSubmit={handleLinkSubmit} />  
     </>
   )
 }
