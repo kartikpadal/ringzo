b5fec6f85865b68b0446aab9c878caa930a3f6a6
kartikpadal <kartikpadal17@gmail.com>
2025-07-23 22:40:30 +0530

    its RingZo now

--- frontend/src/Hero.jsx
+++ frontend/src/Hero.jsx
@@ -1,7 +1,7 @@
 function Hero() {
   return (
     <div className="hero">
-      <h1 className="hero-title">Ringify</h1>
+      <h1 className="hero-title">RingZo</h1>
       <p className="hero-description">
         Instantly get your favorite ringtones in .mp3 format. 
       </p>
